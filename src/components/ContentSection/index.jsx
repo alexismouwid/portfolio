ce38abea1f4d5b4243045bf6745ffafc1150b4db
alexismouwid <alexismouwid@gmail.com>
2025-05-02 15:43:04 -0500

    nuevos colores adaptados a la portfoliosss

--- src/components/ContentSection/index.jsx
+++ src/components/ContentSection/index.jsx
@@ -40,19 +40,15 @@ const ContentSection = forwardRef((props, ref) => {
           <div className="profile-info">
             <h2>Alexis Vega</h2>
             <p className="profile-title">Desarrollador</p>
-
-          </div>
-          {isMobile && (
-                        
-              <div className="skill-item-busquedaMobile">
-                <span className="skill-icon-busquedaMobile">Buscame en</span>
+ <div className="skill-item-busqueda">
+                <span className="buscame-en">Buscame en</span>
                 <a 
                   href="https://www.linkedin.com/in/alexis-vega-3b4437333/" 
                   target="_blank"   
                   rel="noopener noreferrer" 
-                  className='links-redesMobile'> 
+                  className='links-redes'> 
                   <img 
-                    width="80px" height="80px" 
+                    width="45px" height="45px" 
                     src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="Linkedin" 
                     className="skill-icon" 
                   /> 
@@ -61,50 +57,50 @@ const ContentSection = forwardRef((props, ref) => {
                   href="https://github.com/alexismouwid" 
                   target="_blank"   
                   rel="noopener noreferrer" 
-                  className='links-redesMobile'>
+                  className='links-redes'>
                   <img 
-                    width="80px" height="80px" 
+                    width="45px" height="45px" 
                     src="./git.png" alt="Github" 
                     className="skill-icon" 
                   />
                 </a>
-              </div>
-          )}
-        </div>
+              </div> 
+          </div>
+                  </div>
 
         {/* Sección 3: Habilidades */}
         <div className="skills-section">
-          <h3 className="skills-titulo">Software Experience</h3>
+          <h3 className="software-experience">Software Experience</h3>
           <div className="skills-grid">
             {/* Columna 1 */}
             <div className="skill-column">
               <div className="skill-item">
                 <span className="skill-namej">Javascript</span>
-                <img width="30px" height="30px" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="Javascript" className="skill-icon" />
+                <img width="40px" height="40px" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="Javascript" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-nameh">HTML5</span>
-                <img width="30px" height="30px" src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" alt="HTML5" className="skill-icon" />
+                <img width="45px" height="45px" src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" alt="HTML5" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">CSS</span>
-                <img width="30px" height="30px" src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS" className="skill-icon" />
+                <img width="45px" height="45px" src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">React</span>
-                <img width="30px" height="30px" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React" className="skill-icon" />
+                <img width="45px" height="45px" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">Node.js</span>
-                <img width="30px" height="30px" src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" alt="Node.js" className="skill-icon" />
+                <img width="45px" height="45px" src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" alt="Node.js" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">MongoDB</span>
-                <img width="30px" height="30px" src="./mongodb.png" alt="MongoDB" className="skill-icon" />
+                <img width="45px" height="45px" src="./mongodb.png" alt="MongoDB" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">SQL</span>
-                <img width="30px" height="30px" src="./sql.png" alt="SQL" className="skill-icon" />
+                <img width="45px" height="45px" src="./sql.png" alt="SQL" className="skill-icon" />
               </div>
             </div>
 
@@ -112,29 +108,26 @@ const ContentSection = forwardRef((props, ref) => {
             <div className="skill-column">
               <div className="skill-item">
                 <span className="skill-icon">Figma</span>
-                <img width="30px" height="30px" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt="Figma" className="skill-icon" />
+                <img width="45px" height="45px" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt="Figma" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">Github</span>
-                <img width="30px" height="30px" src="./git.png" alt="Github" className="skill-icon" />
+                <img width="45px" height="45px" src="./git.png" alt="Github" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">Postman</span>
-                <img width="30px" height="30px" src="./postman.png" alt="Postman" className="skill-icon" />
+                <img width="45px" height="45px" src="./postman.png" alt="Postman" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">Neovim</span>
-                <img width="30px" height="30px" src="./neovim.png" alt="Nvim" className="skill-icon" />
+                <img width="45px" height="45px" src="./neovim.png" alt="Nvim" className="skill-icon" />
               </div>
               <div className="skill-item">
                 <span className="skill-icon">Linux</span>
-                <img width="30px" height="30px" src="./linux.png" alt="Linux" className="skill-icon" />
+                <img width="45px" height="45px" src="./linux.png" alt="Linux" className="skill-icon" />
               </div>
-              {isMobile === false && (
-
-
-
-              )}           </div>
+            
+            </div>
           </div>
         </div>
       </div>
